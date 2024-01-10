ee4721930c4477feadc3678d840d9b79291f227b
Basmalamoustafa <75669135+Basmalamoustafa@users.noreply.github.com>
2024-01-10 16:54:12 +0200

    Update Cart.cshtml.cs

--- ZCEvents/Pages/Cart.cshtml.cs
+++ ZCEvents/Pages/Cart.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 
-namespace E_commerce.Pages
+namespace ZCEvents.Pages
 {
     public class CartModel : PageModel
     {
