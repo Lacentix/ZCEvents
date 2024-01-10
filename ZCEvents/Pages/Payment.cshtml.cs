308936470cf2af48a35a7086eef07d8ea519c6f0
Basmalamoustafa <75669135+Basmalamoustafa@users.noreply.github.com>
2024-01-10 16:53:46 +0200

    Update Payment.cshtml.cs

--- ZCEvents/Pages/Payment.cshtml.cs
+++ ZCEvents/Pages/Payment.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace E_commerce.Pages
+namespace ZCEvents.Pages
 {
     public class PaymentModel : PageModel
     {
