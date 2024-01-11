1cf5a0893304c080f590949f73f361e220a82f60
mernaahmed2020 <149065043+mernaahmed2020@users.noreply.github.com>
2024-01-11 02:09:21 +0200

    Add files via upload

---
+++ Room1.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZCEvents.Pages;
+
+public class Room1 : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Room3.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZCEvents.Pages;
+
+public class Room3 : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
